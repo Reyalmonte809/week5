d2040688149f786d11e1ff387a01ed678a1025cb
reyalmonte809 <reyalmonte89@gmail.com>
2024-05-06 15:51:56 -0400

    i <3 loops

---
+++ demos/loops.js
@@ -0,0 +1,51 @@
+// while (condition) {
+
+// }
+
+// let num = 1; 
+// let i = 1;
+
+// while(i < 5) {
+//     num = num * 2;
+//     console.log(num);
+//     i++;
+// }
+
+// let num = 1; 
+// let i = 1;
+
+// do {
+//     num = num * 2;
+//     console.log(num);
+//     i++;
+// } while (i < 5)
+
+
+
+
+    // let num = 1
+    // let i  = 1 
+
+    // while (i < 100) {
+    //     num = num * 2;
+    //     if (num >= 100) break;
+    //     i++;
+    // }
+    // console.log(num)
+
+// let hey = "I love loops"
+
+//     for (i = 0; i < 7; i++ ){
+//         console.log(hey)
+//     }
+
+let hi = "I love loops"
+let i = 1;
+
+
+    while (i < 8) {
+        hi = hi;
+        console.log(hi)
+        i++;
+        
+    }
